054e9c16f8e3a741af2c838061a74b4a10521cf1
Pawel Stadnicki <pawel.stadnicki99@gmail.com>
2019-04-12 12:21:54 +0000

    Final Touch Ups

---
+++ action_page.php
@@ -0,0 +1,27 @@
+ <!DOCTYPE html>
+<html>
+    <head>
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.3.1/css/hover-min.css" type="text/css" />
+    	<link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<meta charset="UTF-8">
+    	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    	<title>Of Mice and Men</title>
+        </head>
+    <body>
+       <header>
+            <div class="parallax-3">
+    			<div class="php-content">
+                      <h1>Thank You!</h1>
+                      <p> Thank You for Your contact request. We will be in touch with you shortly.</p>
+                      <p><a class="btn btn-primary btn-lg" href="index.html" role="button">Go back to home page</a></p>
+                </div>
+            </div>
+	    </header>
+    </body>
+    
+</html> 
